10f4d0598d86b44e98f62fccf1b736e249493c1c
amir79 <amir.shahvirdi79@gmail.com>
2022-09-06 09:38:15 +0430

    create model for posts

--- network/models.py
+++ network/models.py
@@ -1,6 +1,18 @@
+from pyexpat import model
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+
+class Posts(models.Model):
+    post_uesr = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True,related_name='create_user')
+    post_title = models.CharField(max_length=200)
+    post_description = models.TextField()
+    post_likes = models.IntegerField()
+    post_date = models.DateField(primary_key=True)
+
+    def __str__(self):
+        return '*'
