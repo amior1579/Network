0e4b971114f6b5f43c08977e523f52c8d05dee33
amir79 <amir.shahvirdi79@gmail.com>
2022-09-06 18:36:44 +0430

    create 'new post' button and add script

---
+++ network/static/network/index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded',function(){
+
+    document.querySelector('.div_add_post').style.display = 'none'
+    
+    document.querySelector('#new_post_button').onclick = ()=>{
+        document.querySelector('.div_add_post').style.display = 'block'
+        
+    }
+
+
+})
