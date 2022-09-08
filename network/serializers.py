7b1e8c0ad3bb00f6f5e89fda56c29fac43718e4d
amir79 <amir.shahvirdi79@gmail.com>
2022-09-08 15:37:32 +0430

    create serializer.py file

---
+++ network/serializers.py
@@ -0,0 +1,8 @@
+from attr import fields
+from rest_framework import serializers
+from .models import *
+
+class PostsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Posts
+        fields = '__all__'
