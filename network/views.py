9b6c496efc420c158eab69b27c934a5766505ba2
amir79 <amir.shahvirdi79@gmail.com>
2022-09-15 11:58:55 +0430

    Redirect to the same page after create new post

--- network/views.py
+++ network/views.py
@@ -181,12 +181,12 @@ def following_posts(request):
 
 
 def add_post(request):
+    user = request.user
     title = request.POST['title']
     description = request.POST['description']
     post = Posts(post_title=title, post_description=description, post_uesr=request.user)
     post.save()
-    return HttpResponseRedirect(reverse('all_posts'))
-    # return JsonResponse({"message": "send post."}, status=201)
+    return redirect('my_account', user=user)
 
 
 
