215cf979829a0ac2e607cbc7591665e77e5b0e31
amir79 <amir.shahvirdi79@gmail.com>
2022-09-15 17:27:34 +0430

    clean code (html,css,js)

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -3,8 +3,7 @@ document.addEventListener('DOMContentLoaded',function(){
     document.querySelector('.div_add_post').style.display = 'none'
     
     document.querySelector('#new_post_button').onclick = ()=>{
-        document.querySelector('.div_add_post').style.display = 'block'
-        
+        document.querySelector('.div_add_post').style.display = 'block'    
     }
     
     const div = document.querySelectorAll('#div_update_description')
@@ -18,40 +17,18 @@ document.addEventListener('DOMContentLoaded',function(){
         json_posts.forEach(posts =>{
             console.log(posts);
 
-
             const edit = document.querySelector(`#edit_button_${posts.id}`)
             if(edit){
                 edit.addEventListener('click', ()=> edit_post(posts), false)
             }
 
-            
-            
-
-            // const edit = document.querySelectorAll('#edit_button')
-            // edit.forEach(button =>{
-            //     button.onclick = ()=>{
-            //         const postId = button.getAttribute('div-id')
-            //         console.log(postId);
-            //         document.querySelector('.div_update_description_' + postId).style.display = 'block'
-                    
-            //         const description = document.querySelector('#post_description_' + postId)
-            //         const inner = description.innerHTML
-            //         console.log(inner);
-
-            //         const form_update = document.querySelector('#form_update')
-            //         form_update.value = inner
-            //         console.log(form_update);
-
-                    
-            //         // edit_post(postId)
-            //     }
-            // }) 
         })
     })
 
     function edit_post(posts){
         document.querySelector(`.div_update_description_${posts.id}`).style.display = 'block'
         document.querySelector(`#post_description_${posts.id}`).style.display = 'none'
+
         const form_update =  document.querySelector(`#form_update_description_${posts.id}`)
         form_update.innerHTML = document.querySelector(`#post_description_${posts.id}`).innerHTML
         console.log(posts.id);
@@ -72,12 +49,5 @@ document.addEventListener('DOMContentLoaded',function(){
 
         })
     }
-
-    // function like_post(posts){
-    //     console.log(posts.id);
-    // }
-
-
-
-
+    
 })
--- network/static/network/like_posts.js
+++ network/static/network/like_posts.js
@@ -11,38 +11,29 @@ document.addEventListener('DOMContentLoaded',function(){
             if(like){
                 like.addEventListener('click', ()=> like_post(posts), false)
             }
-            console.log(like);
-            
         })
     })
 
     function like_post(posts){
-        console.log(posts.id);
-
         fetch('/like',{
             method:'PUT',
             headers: {'Content-Type': 'application/json',},
             body: JSON.stringify ({
                 post: `${posts.id}`,
-                // post: 'aaaaaa'
             })
-
         })
+
         const like =  document.querySelector(`#like_count_${posts.id}`)
-        console.log(parseInt(like.innerHTML));
         const like_num = parseInt(like.innerHTML)
-        console.log(like_num);
         const like_button = document.querySelector(`#like_button_${posts.id}`)
-        console.log(like_button.innerHTML);
+
         if(like_button.innerHTML == 'Like'){
             like.innerHTML = like_num + 1
             like_button.innerHTML = 'Unlike'
-        }
-        else {
+        }else {
             like_button.innerHTML = 'Like'
             like.innerHTML = like_num - 1
         }
-        console.log(like_num + 1);
     }
 
 
